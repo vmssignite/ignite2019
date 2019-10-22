fe77ee73004d21cede78e5fdd035011759176f25
johm-msft <johm@microsoft.com>
2019-10-21 17:06:22 -0700

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/md5 https://raw.githubusercontent.com/catengineering/longlifeapp/master/md5
-/usr/bin/wget -O /opt/server https://github.com/catengineering/longlifeapp/raw/master/server
+/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/master/server
 sleep 5
 chmod +x /opt/server
 /opt/server
