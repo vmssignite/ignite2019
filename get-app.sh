7ac8a1ffce52e27e0416453ffe8c3d4dbc3b727f
johm-msft <johm@microsoft.com>
2019-10-23 10:52:28 -0700

    Update get-app.sh

--- get-app.sh
+++ get-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 size=""
 ulimit -n 20000
-/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/master/server
+/usr/bin/wget -O /opt/server https://github.com/vmssignite/ignite2019/raw/20to90mincrash/server
 sleep 5
 chmod +x /opt/server
 /opt/server
